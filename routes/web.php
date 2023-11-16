f2c550ad292ea1f81421d05dc5504783a4ed15dd
Thomaz980 <thomazrodrigues668@gmail.com>
2023-11-15 23:02:17 -0300

    teste

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,12 @@ use App\Http\Controllers\TurmaController;
 | WEB ROUTES
 |--------------------------------------------------------------------------
 */
+Route::get('/', [AccessibleController::class, 'index'])->name('welcome');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/', [AccessibleController::class, 'index'])->name('welcome');
     Route::get('/logon', [LogonController::class, 'create']);
 
     // CRIAÇÃO DE REQUERIMENTOS
